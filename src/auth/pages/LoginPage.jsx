cff2b1af2c49be6a9a9608d057a43acad1ca47f7
WandirPereira <wandir@gmail.com>
2024-02-26 21:33:18 -0300

    Criando rotas e telas Login. Configuração incial componente Calendar

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,5 +1,64 @@
 import React from "react";
+import "./LoginPage.css";
 
 export const LoginPage = () => {
-  return <h1>LoginPage</h1>;
+  return (
+    <div className="container login-container">
+      <div className="row">
+        <div className="col-md-6 login-form-1">
+          <h3>Acessar</h3>
+          <form>
+            <div className="form-group mb-2">
+              <input type="text" className="form-control" placeholder="Email" />
+            </div>
+            <div className="form-group mb-2">
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Senha"
+              />
+            </div>
+            <div className="d-grid gap-2">
+              <input type="submit" className="btnSubmit" value="Login" />
+            </div>
+          </form>
+        </div>
+
+        <div className="col-md-6 login-form-2">
+          <h3>Registrar</h3>
+          <form>
+            <div className="form-group mb-2">
+              <input type="text" className="form-control" placeholder="Nome" />
+            </div>
+            <div className="form-group mb-2">
+              <input
+                type="email"
+                className="form-control"
+                placeholder="Email"
+              />
+            </div>
+            <div className="form-group mb-2">
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Senha"
+              />
+            </div>
+
+            <div className="form-group mb-2">
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Repita a senha"
+              />
+            </div>
+
+            <div className="d-grid gap-2">
+              <input type="submit" className="btnSubmit" value="Criar conta" />
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,5 +1,52 @@
 import React from "react";
+import { Navbar } from "../components";
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+// import format from "date-fns/format";
+// import parse from "date-fns/parse";
+// import startOfWeek from "date-fns/startOfWeek";
+// import getDay from "date-fns/getDay";
+import ptBR from "date-fns/locale/pt-BR";
+import { addHours, format, parse, startOfWeek, getDay } from "date-fns";
+
+const locales = {
+  "pt-BR": ptBR,
+};
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+});
+
+const events = [
+  {
+    title: "Evento title",
+    notes: "Evento notes",
+    start: new Date(),
+    end: addHours(new Date(), 2),
+    bgColor: "#fafafa",
+    user: {
+      _id: "1234",
+      name: "Wandir",
+    },
+  },
+];
 
 export const CalendarPage = () => {
-  return <h1>CalendarPage</h1>;
+  return (
+    <>
+      <Navbar />
+
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: "calc(100vh - 80px)" }}
+      />
+    </>
+  );
 };
